7aab464090703cc12407dc4865e2746ef102bc53
JIRAUD loic <loic.jiraud@etu.u-paris.fr>
2023-03-25 00:08:02 +0100

    mkdir ls

--- arbre.c
+++ arbre.c
@@ -22,19 +22,48 @@ struct liste_noeud{
 typedef struct noeud noeud;
 typedef struct liste_noeud liste_noeud;
 
-mkdir(noeud *courant, char* nom){
-    assert(courant->est_dossier);
-
+void mkdir(noeud *courant, char* nom){
     noeud *n = malloc(sizeof(noeud));
     n->est_dossier = true,
-    memmove(n->nom, nom, sizeof(char)*strlen(nom));
+    memcpy(n, nom, sizeof(char) * strlen(nom));
     memmove(n->pere, courant, sizeof(noeud));
     memmove(n->racine, courant->racine, sizeof(noeud));
-    //pas besoin d'initié fils, revient à le mettre à NULL
+    memmove(n->fils, NULL, sizeof(liste_noeud));
 
-    //je finirai plus tard
+    if (courant->fils == NULL) memmove(courant->fils, n, sizeof(liste_noeud));
+    else{
+        liste_noeud *l = courant->fils;
+        while (l->suiv != NULL) l = l->suiv;
+        l->suiv = malloc(sizeof(liste_noeud));
+        memmove(l->suiv->noeud, n, sizeof(noeud));
+        memmove(l->suiv->suiv, NULL, sizeof(liste_noeud));
+    }
+
+    free(n);
 }
 
-int main(int nbr, char *argv){
+void ls(noeud *courant){
+    if (courant->fils == NULL) return;
+    liste_noeud *l = courant->fils;
+    while (l != NULL){
+        printf("%s ", l->noeud->nom);
+        l = l->suiv;
+    }
+}
+
+int main(){
+    noeud *racine = malloc(sizeof(noeud));
+    racine->est_dossier = true;
+    memcpy(racine->nom, "/", sizeof(char) * strlen("/"));
+    memmove(racine->pere, NULL, sizeof(noeud));
+    memmove(racine->racine, racine, sizeof(noeud));
+    memmove(racine->fils, NULL, sizeof(liste_noeud));
+
+    mkdir(racine, "home");
+    mkdir(racine, "usr");
+    mkdir(racine, "bin");
+
+    //ls(racine);
+
     return EXIT_SUCCESS;
 }
