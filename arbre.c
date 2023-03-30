1f5fbd82eabf24e729aeb84b6949a395c7656fa4
JIRAUD loic <loic.jiraud@etu.u-paris.fr>
2023-03-30 18:55:10 +0200

    fonction cd

--- arbre.c
+++ arbre.c
@@ -3,13 +3,16 @@
 #include <string.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <ctype.h>
+
+#define MAX_NOM_TAILLE 100
 
 struct noeud;
 struct liste_noeud;
 
 struct noeud{
     bool est_dossier;
-    char nom[100];
+    char nom[MAX_NOM_TAILLE];
     struct noeud *pere; 
     struct noeud *racine;
     struct liste_noeud *fils;
@@ -25,15 +28,24 @@ typedef struct liste_noeud liste_noeud;
 
 void verifNoeud(noeud *n){
     if (n == NULL){
-        printf("allocation null");
+        puts("allocation null");
         assert(n != NULL);
     }
 }
 
 void verifListe(liste_noeud *n){
     if (n == NULL){
-        printf("allocation null");
+        puts("allocation null");
         assert(n != NULL);
+    }
+}
+
+void estAlpha(char *nom){
+    for (int i = 0; i < strlen(nom); i++){
+        if (isalnum(nom[i]) == 0){
+            printf("Le caractere '%c' n'est pas estAlphanumérique.", nom[i]);
+            return;            
+        }
     }
 }
 
@@ -55,24 +67,74 @@ void mkdir(noeud *courant, char* nom){
 }
 
 void ls(noeud *courant){
+    if (!courant->est_dossier) {
+        printf("'%s' n'est pas un dossier.\n", courant->nom);
+        return;
+    }
+    liste_noeud *l = courant->fils;
+    while(l != NULL){
+        printf("%s\n", l->noeud->nom);
+        l = l->suiv;
+    }
+    free(l);
 }
 
+noeud* cd(noeud *courant, char *chemin) {
+    if (!courant->est_dossier || courant->fils == NULL) return NULL;
+    if (chemin[0] == '/'){
+        courant = courant->racine;
+        if (strlen(chemin) < 2) return courant;     // "cd /"
+        chemin++;
+    }
+    char* next = strchr(chemin, '/');
+    liste_noeud* liste = courant->fils;
+    if (next == NULL){
+        while (liste != NULL){
+            if (strcmp(liste->noeud->nom, chemin) == 0) return liste->noeud;    // "cd exemple"
+            liste = liste->suiv;
+        }
+        return NULL;
+    }
+    int len = strlen(chemin) - strlen(next); 
+    char* premier_mot = malloc(sizeof(char)*len);
+    memmove(premier_mot, chemin, sizeof(char)*len);
+    while(liste != NULL){
+        if (strcmp(liste->noeud->nom, premier_mot) == 0)
+            return cd(liste->noeud, next);
+        liste = liste->suiv;
+    }
+    return NULL;
+}
+
+
+
+//int main(int nbr, char *args) //fichier texte + scanf("..")
 int main(){
     noeud *racine = malloc(sizeof(noeud));
     verifNoeud(racine);
     racine->est_dossier = true;
-    memcpy(racine->nom, "/", sizeof(char) * 1);
+    memcpy(racine->nom, "", sizeof(char) * strlen(""));
     racine->pere = NULL;
     racine->racine = racine;
     racine->fils = NULL;
 
-    mkdir(racine, "dossier1");
-    mkdir(racine, "dossier2");
-    mkdir(racine, "dossier3");
+    char *cours = "Cours";
+    char *td = "Td";
+    char *edt = "Edt";
+    char *projetC = "ProjetC";
+    char *anglais = "Anglais";
+    char *td1 = "TD1";
+    char *td2 = "TD2";
 
+    mkdir(racine, cours);
+    mkdir(racine, td);
+    mkdir(racine, edt);
+    //ls(racine);
+
+    racine = cd(racine, cours);
+    mkdir(racine, projetC);
+    mkdir(racine, anglais);
     ls(racine);
-
-    free(racine);
 
     return EXIT_SUCCESS;    
 }
